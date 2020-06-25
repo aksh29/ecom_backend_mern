c09da683c95ba3255e051548d8012a4e7bb6a941
Akshat Pandey <akshat.pandey383@gmail.com>
2020-06-25 19:45:02 +0530

    commit

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-mongoose.connect(process.env.DATABASE, 
-{
+const authRoutes = require("./routes/auth");
+
+//DB Connection
+mongoose
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex:true
-}).then(() => {
-    console.log("DB is connected");
-});
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
 
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
+//My Routes
+app.use("/api", authRoutes);
+
+//PORT
 const port = process.env.PORT || 8000;
 
-app.listen(port , () => {
-    console.log(`app is running at ${port}`);
-})
+//Starting a server
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
